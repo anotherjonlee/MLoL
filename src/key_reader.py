db2ef48a3b3c10922d62241bf97131bccfa15139
Jonathan Lee <anotherjonlee@MacBook-Air-2.local>
2020-05-09 22:51:24 -0700

    last update for the day

--- src/key_reader.py
+++ src/key_reader.py
@@ -1,9 +1,12 @@
 def key():
-
+    """
+    output: returns an api key from the config.ini file
+    """
+    
     from configparser import ConfigParser
     
     config = ConfigParser()
 
-    config.read('src/config.ini')
+    config.read('../src/config.ini')    
     
     return config['api']['key']
