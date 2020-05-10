38aea5f6a0602fdabf4cc420a2229d76f9ad9532
Jonathan Lee <anotherjonlee@MacBook-Air-2.local>
2020-05-09 22:20:57 -0700

    adding an estimator function in the pipeline file

--- src/pipeline.py
+++ src/pipeline.py
@@ -1,11 +1,6 @@
-from sklearn.preprocessing import StandardScaler, OneHotEncoder, LabelEncoder
-from sklearn.pipeline import Pipeline
-from sklearn.compose import ColumnTransformer
-from sklearn.impute import SimpleImputer
-
-def pipeline(numeric_features = False,      \
-             numeric_feature_names = [],    \
-             cat_features = False,          \
+def pipeline(numeric_features = False, 
+             numeric_feature_names = [],
+             cat_features = False,
              cat_feature_names = []):
     """
     The function takes names of numeric and categorical features
@@ -14,12 +9,15 @@ def pipeline(numeric_features = False,      \
     
     input:  numeric features in df (bool)
             categorical features in df (bool)
-            names of numeric features (list)
-            names of categorical features (list)   
+            **kargs ({'num_features': (list),'cat_features':(list)})   
     
     output: Pipeline object with imputed and scaled numeric features
             and imputed and one hot encoded categorical features      
     """
+    from sklearn.preprocessing import StandardScaler, OneHotEncoder, LabelEncoder
+    from sklearn.pipeline import Pipeline
+    from sklearn.compose import ColumnTransformer
+    from sklearn.impute import SimpleImputer
     
     base_transformer = ColumnTransformer(transformers=[])
 
@@ -31,7 +29,9 @@ def pipeline(numeric_features = False,      \
         ])
         
         # Append the numeric pipeline to the base_transformer
-        base_transformer.transformers.append(('numeric',num_processor,numeric_feature_names))
+        base_transformer.transformers.append(('numeric',
+                                              num_processor,
+                                              numeric_feature_names))
         
     if cat_features == True:
         # Categorical features pipeline
@@ -39,14 +39,34 @@ def pipeline(numeric_features = False,      \
             ('cat_imputer',SimpleImputer(strategy='most_frequent')),
             ('encoder',OneHotEncoder(handle_unknown='ignore'))
         ])
+        
         # Append the categorical pipeline to the base_transformer
-        base_transformer.transformers.append(('categorical',cat_processor,cat_feature_names))
+        base_transformer.transformers.append(('categorical',
+                                              cat_processor,
+                                              cat_feature_names))
 
     # Final base pipeline
     if numeric_features == True or cat_features == True:
-        base_pipeline = Pipeline(steps=[('transformer',base_transformer)])
+        base_pipeline = Pipeline(steps=[('transformer',
+                                         base_transformer)])
     
     else:
+        print('function failed')
         base_pipeline = Pipeline(steps = [])
     
     return base_pipeline
+
+def append_model(estimator,base_pipeline):
+    """
+    input:  machine learning estimator method, 
+            pipeline object
+    
+    output: pipeline object with an appended estimator
+    """
+    
+    from sklearn.base import clone
+    
+    pipeline = clone(base_pipeline)
+    pipeline.steps.append(('estimator', estimator))
+    
+    return pipeline
