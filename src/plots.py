06f9613daf721ff110ba5ddf6dd6d727e2b4d245
Jonathan Lee <anotherjonlee@MacBook-Air-2.local>
2020-05-13 15:40:33 -0700

    resolving indentation issue

--- src/plots.py
+++ src/plots.py
@@ -1,19 +1,14 @@
 def plot_cm(ax, estimator, X_test, y_test):
     
-    from sklearn.metrics import plot_confusion_matrix
+from sklearn.metrics import plot_confusion_matrix
 
-    # Plot non-normalized confusion matrix
-    titles_options = [("Confusion matrix, without normalization", None),
-                    ("Normalized confusion matrix", 'true')]
-    
-    for title, normalize in titles_options:
-        disp = plot_confusion_matrix(classifier, X_test, y_test,
-                                    display_labels=class_names,
-                                    cmap=plt.cm.Blues,
-                                    normalize=normalize)
-        disp.ax_.set_title(title)
 
-        print(title)
-        print(disp.confusion_matrix)
+disp = plot_confusion_matrix(classifier, X_test, y_test,
+                            display_labels=class_names,
+                            cmap=plt.cm.Blues,
+                            normalize=normalize)
+disp.ax_.set_title(title)
+print(title)
+print(disp.confusion_matrix)
 
-    plt.show()
+plt.show()
