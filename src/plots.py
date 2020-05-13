5f557d0c8c52b30ce24bd8c9bd845ec166e1a389
Jonathan Lee <anotherjonlee@MacBook-Air-2.local>
2020-05-13 16:02:43 -0700

    adding additional functions

--- src/plots.py
+++ src/plots.py
@@ -1,14 +1,68 @@
 def plot_cm(ax, estimator, X_test, y_test):
     
-from sklearn.metrics import plot_confusion_matrix
+    """
+    input:  a plt.subplot object, an estimator object, X_test array, y_test array
+    output: a confusion matrix plot
+    """
+    
+    from sklearn.metrics import plot_confusion_matrix
+    import matplotlib.pyplot as plt
+    import seaborn as sns
+    %matplotlib inline
+    plt.style.use('ggplot')
 
-
-disp = plot_confusion_matrix(classifier, X_test, y_test,
-                            display_labels=class_names,
-                            cmap=plt.cm.Blues,
-                            normalize=normalize)
-disp.ax_.set_title(title)
-print(title)
-print(disp.confusion_matrix)
-
-plt.show()
+    disp = plot_confusion_matrix(classifier, X_test, y_test,
+                                display_labels=class_names,
+                                cmap=plt.cm.Blues,
+                                normalize=normalize)
+    plt.show()
+    
+def general_correlation_plot(ax,df):
+    
+    """
+    input:  a plt.subplot object and a pandas dataframe
+    output: a correlation plot 
+    """
+    
+    import matplotlib.pyplot as plt
+    import seaborn as sns
+    %matplotlib inline
+    plt.style.use('ggplot')
+    
+    ax = sns.pairplot(df,
+                      corner = True,
+                      hue = 'teams')
+    plt.show()
+    
+def granular_correlation_plot(ax,df, var1, var2, var3 = None):
+    
+    """
+    input:  a plt.subplot object, a pandas dataframe, feature names (string)
+    output: a correlation plot 
+    """
+    
+    import matplotlib.pyplot as plt
+    import seaborn as sns
+    %matplotlib inline
+    plt.style.use('ggplot')
+    
+    if var3 == None:
+        ax = sns.pairplot(df,
+                        corner = True,
+                        hue = 'teams',
+                        vars=[var1, var2])
+        plt.show()
+    else:
+        ax = sns.pairplot(df,
+                        corner = True,
+                        hue = 'teams',
+                        vars=[var1, var2, var3])
+        plt.show()
+        
+def box_plot(ax, df, x, y, hue_var = None):
+    ax = sns.boxplot(x = x, 
+                     y = y, 
+                     data = df,
+                     hues = hue_var,
+                     orient = 'h')
+    plt.show()
